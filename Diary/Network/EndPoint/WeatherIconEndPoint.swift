d4da7bfbe290148a44ecab41369e9e430adc6467
h-suo <phstoo1021@naver.com>
2023-11-10 16:31:19 +0900

    ✨Feat: WeatherIconEndPoint 추가

---
+++ Diary/Network/EndPoint/WeatherIconEndPoint.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherIconEndPoint.swift
+//  Diary
+//
+//  Created by Erick on 2023/11/10.
+//
+
+import Foundation
+
+struct WeatherIconEndPoint: NetworkConfigurable {
+    
+    // MARK: - DTO Type
+    typealias Response = Data
+    
+    // MARK: - Property
+    var baseURL: String
+    var path: String
+    var method: HttpMethod
+    var queryParameters: [String: String]?
+    
+    // MARK: - Initializer
+    init(baseURL: String = "https://openweathermap.org",
+         path: String = "img/wn",
+         method: HttpMethod = .get,
+         iconId: String
+    ) {
+        self.baseURL = baseURL
+        self.path = path + "/" + iconId + "@2x.png"
+        self.method = method
+        self.queryParameters = nil
+    }
+}
