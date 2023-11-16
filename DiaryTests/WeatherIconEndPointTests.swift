151404590b55883846b72c5fe68ec1d646e8b326
h-suo <phstoo1021@naver.com>
2023-11-16 17:16:51 +0900

    ✅Test: 엔드포인트 테스트 추가

---
+++ DiaryTests/WeatherIconEndPointTests.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherIconEndPointTests.swift
+//  DiaryTests
+//
+//  Created by Erick on 2023/11/16.
+//
+
+import XCTest
+import CoreData
+@testable import Diary
+
+final class WeatherIconEndPointTests: XCTestCase {
+
+    // MARK: - Test Case
+    func test_WeatherIconEndPoint의_url을_호출하면_WeatherConditionAPI_URL을_반환합니다() {
+        do {
+            // given
+            let expectation = URL(string: "https://openweathermap.org/img/wn/10d@2x.png")
+            let endPoint = WeatherIconEndPoint(iconId: "10d")
+            // when
+            let result = try endPoint.url()
+            // then
+            XCTAssertEqual(result, expectation)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+}
