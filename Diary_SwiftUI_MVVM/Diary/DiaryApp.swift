c1dafa0810406c7f9bd25577a5d26cf7d908d5d0
h-suo <phstoo1021@naver.com>
2024-01-15 14:55:39 +0900

    🚚build: Diary_SwiftUI_MVVM 파일 추가

---
+++ Diary_SwiftUI_MVVM/Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by Erick on 2024/01/15.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
