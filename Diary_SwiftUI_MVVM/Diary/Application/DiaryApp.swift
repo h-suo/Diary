cbf75908cfbfc1aa2133a43417c4dddafe5eb1b4
h-suo <phstoo1021@naver.com>
2024-01-15 16:17:11 +0900

    ✨Feat: App, Domain 그룹 추가 및 Diary 추가

--- Diary_SwiftUI_MVVM/Diary/Application/DiaryApp.swift
+++ Diary_SwiftUI_MVVM/Diary/Application/DiaryApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct DiaryApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      DiaryListView(diarys: [])
     }
+  }
 }
---
+++ Diary_SwiftUI_MVVM/Diary/Domain/Diary.swift
@@ -0,0 +1,31 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Erick on 2024/01/15.
+//
+
+import Foundation
+
+struct Diary: Identifiable {
+  var id: UUID
+  var title: String
+  var contents: String
+  var date: Date
+  var dateString: String {
+    date.description
+  }
+  
+  // MARK: - Initializer
+  init(
+    id: UUID = UUID(),
+    title: String,
+    contents: String,
+    date: Date = Date()
+  ) {
+    self.id = id
+    self.title = title
+    self.contents = contents
+    self.date = date
+  }
+}
