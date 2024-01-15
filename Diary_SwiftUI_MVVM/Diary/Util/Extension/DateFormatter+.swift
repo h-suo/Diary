89e104389fb796c6d7ff852fa36e5eb4c3358d54
h-suo <phstoo1021@naver.com>
2024-01-15 16:40:48 +0900

    ✨Feat: DateFormatter에 currentLong static 프로퍼티 추가

---
+++ Diary_SwiftUI_MVVM/Diary/Util/Extension/DateFormatter+.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatter+.swift
+//  Diary
+//
+//  Created by Erick on 2024/01/15.
+//
+
+import Foundation
+
+extension DateFormatter {
+  static let currentLong: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale.current
+    dateFormatter.timeZone = TimeZone.current
+    dateFormatter.dateStyle = .long
+    
+    return dateFormatter
+  }()
+}
