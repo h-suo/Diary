99610af9f65addf72d3bcd61b8064a689abbb0ee
h-suo <phstoo1021@naver.com>
2024-01-18 19:41:46 +0900

    ✨Feat: PersistenceController 추가

---
+++ Diary_SwiftUI_MVVM/Diary/Application/PersistenceController.swift
@@ -0,0 +1,54 @@
+//
+//  PersistenceController.swift
+//  Diary
+//
+//  Created by Erick on 2024/01/18.
+//
+
+import CoreData
+
+struct PersistenceController {
+  
+  static let shared = PersistenceController()
+  
+  static var preview: PersistenceController = {
+    let result = PersistenceController(inMemory: true)
+    let viewContext = result.container.viewContext
+    
+    for _ in 0..<10 {
+      let newDiaryDTO = DiaryDTO(context: viewContext)
+      newDiaryDTO.id = UUID()
+      newDiaryDTO.title = "Title"
+      newDiaryDTO.contents = "Contents"
+      newDiaryDTO.date = Date()
+    }
+    
+    do {
+      try viewContext.save()
+    } catch {
+      let nsError = error as NSError
+      fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    }
+    
+    return result
+  }()
+  
+  let container: NSPersistentContainer
+  
+  init(inMemory: Bool = false) {
+    
+    container = NSPersistentContainer(name: "Diary")
+    
+    if inMemory {
+      container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+    }
+    
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    
+    container.viewContext.automaticallyMergesChangesFromParent = true
+  }
+}
