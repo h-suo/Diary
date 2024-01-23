173874f8d2453fa3aa3149cfd8a853025415e500
h-suo <phstoo1021@naver.com>
2024-01-23 15:07:17 +0900

    ✨Feat: swipeAction, shareLink 추가 및 View, Link 분리

--- Diary_SwiftUI_MVVM/Diary/Presentation/DiaryList/View/DiaryListView.swift
+++ Diary_SwiftUI_MVVM/Diary/Presentation/DiaryList/View/DiaryListView.swift
@@ -14,44 +14,59 @@ struct DiaryListView: View {
   
   var body: some View {
     NavigationStack {
-      List {
-        ForEach(viewModel.diarys) { diary in
-          NavigationLink {
-            DiaryDetailView()
-              .environmentObject(
-                DIContainer
-                  .shared
-                  .diaryDetailViewModel(diary: diary)
-              )
-          } label: {
-            DiaryRowView(diary: diary)
-          }
+      List(viewModel.diarys) { diary in
+        diaryDetailViewLink(diary)
+        .swipeActions {
+          deleteButton(diary)
+          diaryShareLink(diary)
         }
-        .onDelete(perform: viewModel.deleteDiary)
       }
       .listStyle(.plain)
       .navigationTitle("Diary")
       .navigationBarTitleDisplayMode(.inline)
-      .toolbar {
-        ToolbarItem {
-          NavigationLink {
-            DiaryDetailView()
-              .environmentObject(
-                DIContainer
-                  .shared
-                  .diaryDetailViewModel(diary: nil)
-              )
-          } label: {
-            Image(systemName: "plus")
-          }
-        }
-      }
-      .onAppear {
-        viewModel.fetchDiarys()
-      }
+      .toolbar { ToolbarItem { diaryDetailViewLink } }
+      .onAppear { viewModel.fetchDiarys() }
     }
     .alert(isPresented: $viewModel.isError) {
       Alert(title: Text(viewModel.errorMessage))
+    }
+  }
+  
+  private func diaryDetailViewLink(_ diary: Diary) -> some View {
+    NavigationLink {
+      DiaryDetailView()
+        .environmentObject(
+          DIContainer
+            .shared
+            .diaryDetailViewModel(diary: diary)
+        )
+    } label: {
+      DiaryRowView(diary: diary)
+    }
+  }
+  
+  private func deleteButton(_ diary: Diary) -> some View {
+    Button("Delete", role: .destructive) {
+      viewModel.deleteDiary(diary)
+    }
+  }
+  
+  private func diaryShareLink(_ diary: Diary) -> some View {
+    ShareLink(item: viewModel.shareItem(diary)) {
+      Text("Share")
+    }
+  }
+  
+  private var diaryDetailViewLink: some View {
+    NavigationLink {
+      DiaryDetailView()
+        .environmentObject(
+          DIContainer
+            .shared
+            .diaryDetailViewModel(diary: nil)
+        )
+    } label: {
+      Image(systemName: "plus")
     }
   }
 }
