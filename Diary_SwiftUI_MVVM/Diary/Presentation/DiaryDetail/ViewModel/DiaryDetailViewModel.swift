e269079562272c51a3a8e3f9d30a830ddf983b8b
h-suo <phstoo1021@naver.com>
2024-01-23 15:08:41 +0900

    ✨Feat: isNew 프로퍼티 및 deleteDiary, shareItem 함수 추가

--- Diary_SwiftUI_MVVM/Diary/Presentation/DiaryDetail/ViewModel/DiaryDetailViewModel.swift
+++ Diary_SwiftUI_MVVM/Diary/Presentation/DiaryDetail/ViewModel/DiaryDetailViewModel.swift
@@ -10,14 +10,17 @@ import Foundation
 final class DiaryDetailViewModel: ObservableObject {
   
   @Published var diary: Diary
-  @Published var isBack: Bool = false
   @Published var isError: Bool = false
-  @Published var errorMessage: String = ""
+  
+  private(set) var errorMessage: String = ""
+  private(set) var isBack: Bool = false
+  private(set) var isNew: Bool
   
   private var useCase: DiaryUseCase
   
   init(diary: Diary?, useCase: DiaryUseCase) {
     self.diary = diary ?? Diary(title: "", contents: "")
+    self.isNew = diary == nil ? true : false
     self.useCase = useCase
   }
   
@@ -35,5 +38,19 @@ final class DiaryDetailViewModel: ObservableObject {
       errorMessage = error.localizedDescription
       isError = true
     }
+  }
+  
+  func deleteDiary() {
+    do {
+      try useCase.delete(diary)
+      isBack = true
+    } catch {
+      errorMessage = error.localizedDescription
+      isError = true
+    }
+  }
+  
+  func shareItem() -> String {
+    String(format: "%@\n%@", diary.title, diary.contents)
   }
 }
