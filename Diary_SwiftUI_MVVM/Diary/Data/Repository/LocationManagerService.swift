aac06cd6f3087bd07825b700dad704081ef21fd1
h-suo <phstoo1021@naver.com>
2024-01-26 17:57:01 +0900

    ✨Feat: LocationManagerService 추가

---
+++ Diary_SwiftUI_MVVM/Diary/Data/Repository/LocationManagerService.swift
@@ -0,0 +1,45 @@
+//
+//  LocationManagerService.swift
+//  Diary
+//
+//  Created by Erick on 2024/01/26.
+//
+
+import Combine
+import CoreLocation
+
+final class LocationManagerService: NSObject, CLLocationManagerDelegate, LocationService {
+  
+  private var locationManager = CLLocationManager()
+  private var locationSubject = PassthroughSubject<CLLocationCoordinate2D, LocationServiceError>()
+  
+  override init() {
+    super.init()
+    
+    locationManager.delegate = self
+    locationManager.requestWhenInUseAuthorization()
+  }
+  
+  func requestLoactionPublisher() -> AnyPublisher<CLLocationCoordinate2D, LocationServiceError> {
+    locationManager.requestLocation()
+    return locationSubject.eraseToAnyPublisher()
+  }
+  
+  func locationManager(
+    _ manager: CLLocationManager,
+    didUpdateLocations locations: [CLLocation]
+  ) {
+    guard let location = locations.last?.coordinate
+    else { return }
+    
+    locationSubject.send(location)
+    locationManager.stopUpdatingLocation()
+  }
+  
+  func locationManager(
+    _ manager: CLLocationManager,
+    didFailWithError error: Error
+  ) {
+    locationSubject.send(completion: .failure(LocationServiceError.requestLocation(error)))
+  }
+}
