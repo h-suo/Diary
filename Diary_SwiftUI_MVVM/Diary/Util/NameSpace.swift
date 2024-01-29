94d2c6917baae8792bf743511a3117e9e87ba1e4
h-suo <phstoo1021@naver.com>
2024-01-29 14:03:52 +0900

    ♻️Refactor: 실패 메시지 추가

--- Diary_SwiftUI_MVVM/Diary/Util/NameSpace.swift
+++ Diary_SwiftUI_MVVM/Diary/Util/NameSpace.swift
@@ -7,12 +7,17 @@
 
 enum NameSpace {
   static let diary = "Diary"
+  static let diaryDTO = "DiaryDTO"
   static let title = "Title"
   static let contents = "Contents"
   static let delete = "Delete"
   static let share = "Share"
   static let enterTitle = "Enter a title."
-  static let diaryDTO = "DiaryDTO"
+  static let weatherFetchFailed = "Failed to fetch weather information."
+  static let diaryFetchFailed = "Failed to fetch diarys."
+  static let diaryUpdateFailed = "Failed to update diary."
+  static let diaryDeletionFailed = "Failed to delete diary."
+  
   static let testWeatherID = "04n"
   static let inMemoryPath = "/dev/null"
   
